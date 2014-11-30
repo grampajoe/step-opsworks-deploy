298a4fa09fa5609d134c847be38f8a7461aa3237
Joe Friedl <joe@joefriedl.net>
2014-11-29 21:32:02 -0500

    Display argument errors nicely

--- bin/deploy.js
+++ bin/deploy.js
@@ -3,4 +3,17 @@
 var cli = require('../lib/cli'),
     deploy = require('../lib/deploy');
 
-cli(process.argv, deploy);
+try {
+  cli(process.argv, deploy);
+}
+catch(err) {
+  // Display argument required errors nicely
+  if (err.message.match(/--[a-zA-Z-]+ is required/)) {
+    console.error();
+    console.error('  error: ' + err.message);
+    console.error();
+    process.exit(1);
+  } else {
+    throw err;
+  }
+}
