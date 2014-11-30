4e5c9c0e674ce4a601295e46e70aa7859b94f5f4
Joe Friedl <joe@joefriedl.net>
2014-11-30 18:30:56 -0500

    Don't catch errors in the cli, that's too leaky

--- bin/deploy.js
+++ bin/deploy.js
@@ -3,17 +3,4 @@
 var cli = require('../lib/cli'),
     deploy = require('../lib/deploy');
 
-try {
-  cli(process.argv, deploy);
-}
-catch(err) {
-  // Display argument required errors nicely
-  if (err.message.match(/--[a-zA-Z-]+ is required/)) {
-    console.error();
-    console.error('  error: ' + err.message);
-    console.error();
-    process.exit(1);
-  } else {
-    throw err;
-  }
-}
+cli(process.argv, deploy);
