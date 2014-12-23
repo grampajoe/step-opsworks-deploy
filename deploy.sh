999228650cc0f171588be214d336525738d5b93b
Joe Friedl <joe@joefriedl.net>
2014-12-23 14:29:30 -0500

    Add the wait-for-deploy option

--- deploy.sh
+++ deploy.sh
@@ -36,14 +36,16 @@ fi
 
 if [ "$WERCKER_OPSWORKS_DEPLOY_MIGRATE" = "true" ]; then
     export AWS_OPSWORKS_MIGRATE="--migrate";
-else
-    export AWS_OPSWORKS_MIGRATE="";
 fi
 
 if [ ! -n "$WERCKER_OPSWORKS_DEPLOY_COMMENT" ]; then
     export DEPLOY_COMMENT="Wercker deploy by $WERCKER_STARTED_BY.";
 else
     export DEPLOY_COMMENT="$WERCKER_OPSWORKS_DEPLOY_COMMENT";
+fi
+
+if [ "$WERCKER_OPSWORKS_DEPLOY_WAIT_FOR_DEPLOY" = "true" ]; then
+    export AWS_OPSWORKS_WAIT_FOR_DEPLOY="--wait-for-deploy";
 fi
 
 info 'Deploying...';
@@ -54,4 +56,5 @@ dropper opsworks \
     --app-id $AWS_OPSWORKS_APP_ID \
     --region $AWS_DEFAULT_REGION \
     $AWS_OPSWORKS_MIGRATE \
+    $AWS_OPSWORKS_WAIT_FOR_DEPLOY \
     --comment "$DEPLOY_COMMENT";
