a6bd0bcc7cdf68637a48585cb644c4eb7a441b78
Joe Friedl <joe@joefriedl.net>
2015-01-25 14:23:10 -0500

    Check for Node before installing it

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-info 'Installing Node...';
-sudo apt-get update -y;
-sudo apt-get install nodejs -y;
+if ! node -v > /dev/null; then
+    info 'Installing Node...';
+    sudo apt-get install nodejs -y;
+fi
 
 info 'Installing the CLI...';
 sudo npm install -g dropper@1.1.x
